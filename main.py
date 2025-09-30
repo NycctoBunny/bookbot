c43ddc35475778e3a49c13f41688e7096f852b9c
NycctoBunny <nicholas.r612@gmail.com>
2025-09-29 22:58:56 -0500

    completed bookbot project

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import sys
+
+if len(sys.argv) < 2:
+	print(f"Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+
+from stats import (
+	get_num_words,
+	chars_dict_to_sorted_list,
+	get_chars_dict,
+)
+
 def main():
-	with open("books/frankenstein.txt") as file:
-		file_contents = file.read()
-		print(file_contents)
+	book_path = sys.argv[1]
+	text = get_book_text(book_path)
+	num_words = get_num_words(text)
+	chars_dict = get_chars_dict(text)
+	chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+	print_report(book_path, num_words, chars_sorted_list)
+
+def get_book_text(path):
+	with open(path) as f:
+		return f.read()
+
+def print_report(book_path, num_words, chars_sorted_list):
+	print(book_path)
+	print(num_words)
+	for item in chars_sorted_list:
+		if not item ["char"].isalpha():
+			continue
+		print(f"{item['char']}: {item['num']}")
 
 main()
